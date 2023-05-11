3940ae2969c91ed855e23964642e7553914760e7
gianniskontaxis <gianniskontaxis23@yahoo.com>
2023-05-11 16:20:17 +0300

    Php file , with a test

--- logHistory.php
+++ logHistory.php
@@ -1,17 +1,26 @@
 <?php
  $data = array();
- $FullName = $_GET["FullName"];
- $email = $_GET["email"];
- $AMKA = $_GET["AMKA"];
  $host="localhost";
  $uname="root";
  $pass="";
- $dbname="physio";
+ $dbname="patients";
+ 
  $dbh = mysqli_connect($host,$uname,$pass) or die("cannot connect");
  mysqli_select_db($dbh, $dbname);
- $sql = "INSERT into xrhstes values('" . $FullName . "','" . $email . "','" .
-$AMKA . "')";
- echo $sql;
- mysqli_query($dbh, $sql);
- mysqli_close($dbh);
+
+
+ $sql = "SELECT first_name, last_name FROM patients";
+ $result = mysqli_query($dbh, $sql);
+
+/*// Test to see if the php gets correctly the data
+
+ while ($row = mysqli_fetch_assoc($result)) {
+    echo "first_name: " . $row["first_name"] . "<br>";
+    echo "last_name: " . $row["last_name"] . "<br>";
+  }
+  */
+
+$patients = mysqli_fetch_all($result,MYSQLI_ASSOC);
+echo $patients;
+mysqli_close($dbh);
 ?>
