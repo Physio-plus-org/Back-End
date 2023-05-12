f7f9384e2df36df0c4888565e40f1080a8d800c4
gianniskontaxis <gianniskontaxis23@yahoo.com>
2023-05-12 13:33:35 +0300

    Correct php file

--- logHistory.php
+++ logHistory.php
@@ -8,16 +8,18 @@
  $dbh = mysqli_connect($host,$uname,$pass) or die("cannot connect");
  mysqli_select_db($dbh, $dbname);
 
+ $sql = "SELECT brand, GROUP_CONCAT(model) AS grouped_models,
+GROUP_CONCAT(image) as images FROM models GROUP BY brand";
 
- $sql = "SELECT first_name, last_name FROM patients";
+ $sql = "SELECT  GROUP_CONCAT(first_name) AS FirstNames , GROUP_CONCAT(last_name) AS LastNames FROM patients";
 
   $result = mysqli_query($dbh, $sql);
 
   
     while($row = mysqli_fetch_array($result)) {
         $output = array();
-        $output['first_name'] = $row['first_name'];
-        $output['last_name'] = $row['last_name'];
+        $output['FirstNames'] = $row['FirstNames'];
+        $output['LastNames'] = $row['LastNames'];
     }
 
 
