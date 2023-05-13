ef58033ef9f97208d1b269004351b19c54f2b080
gianniskontaxis <gianniskontaxis23@yahoo.com>
2023-05-13 18:41:16 +0300

    Delete non-useful stuff

--- logHistory.php
+++ logHistory.php
@@ -8,8 +8,7 @@
  $dbh = mysqli_connect($host,$uname,$pass) or die("cannot connect");
  mysqli_select_db($dbh, $dbname);
 
- $sql = "SELECT brand, GROUP_CONCAT(model) AS grouped_models,
-GROUP_CONCAT(image) as images FROM models GROUP BY brand";
+
 
  $sql = "SELECT  GROUP_CONCAT(first_name) AS FirstNames , GROUP_CONCAT(last_name) AS LastNames FROM patients";
 
