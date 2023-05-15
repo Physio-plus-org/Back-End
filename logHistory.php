1168714211ccd263bc362d4be6bf4cf7a702122f
ChrisGiamakidis <giamaros4@gmail.com>
2023-05-15 19:19:13 +0300

    Delete previous commit

--- logHistory.php
+++ logHistory.php
@@ -1,17 +1,3 @@
 <?php
- $data = array();
- $fullname = $_GET["fullname"];
- $amka = $_GET["amka"];
- $address = $_GET["address"];
- $host="localhost";
- $uname="root";
- $pass="";
- $dbname="patient";
- $dbh = mysqli_connect($host,$uname,$pass) or die("cannot connect");
- mysqli_select_db($dbh, $dbname);
- $sql = "INSERT into dataofpatient values('" . $fullname . "','" . $amka . "','" .
-$address . "')";
- echo $sql;
- mysqli_query($dbh, $sql);
- mysqli_close($dbh);
+    
 ?>
