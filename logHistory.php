6d4119f5801daf687ed289a57abe304ba1c7d429
gianniskontaxis <gianniskontaxis23@yahoo.com>
2023-05-28 19:16:14 +0300

    Newest Back-End
    Updated php file

--- logHistory.php
+++ logHistory.php
@@ -1,5 +1,5 @@
 <?php
- $data = array();
+ $output = array();
  $host="localhost";
  $uname="root";
  $pass="";
@@ -8,22 +8,21 @@
  $dbh = mysqli_connect($host,$uname,$pass) or die("cannot connect");
  mysqli_select_db($dbh, $dbname);
 
-
-
- $sql = "SELECT  GROUP_CONCAT(first_name) AS FirstNames , GROUP_CONCAT(last_name) AS LastNames, GROUP_CONCAT(soc_sec_reg_num) AS AMKA FROM patients";
+ $sql = "SELECT * FROM patients";
 
   $result = mysqli_query($dbh, $sql);
 
-  
-    while($row = mysqli_fetch_array($result)) {
-        $output = array();
-        $output['FirstNames'] = $row['FirstNames'];
-        $output['LastNames'] = $row['LastNames'];
-        $output['AMKA'] = $row['AMKA'];
+  if ($result->num_rows > 0) {
+    while($row = $result -> fetch_assoc()) {
+        $output[] = $row;    
     }
 
+    $json = json_encode($output);
+    echo $json;
+}
 
+   
 mysqli_close($dbh);
 header("Content-Type: application/json");
-echo json_encode($output); 
+ 
 ?>
