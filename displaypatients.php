e2d703aa9778cf2bba6de5ef84ca6823b847b786
NikosSakell <nikossakell02@gmail.com>
2023-05-31 23:41:28 +0300

    trying different patients

--- displaypatients.php
+++ displaypatients.php
@@ -8,7 +8,7 @@ $dbname = "physio_app";
 $dbh = mysqli_connect($host, $uname, $pass, $dbname) or die("Cannot connect to the database.");
 mysqli_set_charset($dbh, "utf8");
 
-$sql = "SELECT CONCAT(first_name,' ', last_name) AS name, age, address FROM patients WHERE soc_sec_reg_num = '279869537922'";
+$sql = "SELECT CONCAT(first_name,' ', last_name) AS name, age, address FROM patients WHERE soc_sec_reg_num = '287261045727'";
 
 $result = mysqli_query($dbh, $sql);
 
