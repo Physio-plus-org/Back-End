d5a2c33896fed06a7a38c29ead0b604e4d725ba2
NikosSakell <nikossakell02@gmail.com>
2023-06-02 18:55:59 +0300

    modified json objects
    modified json object to return as arrays of sessions per patient

--- displaypatients.php
+++ displaypatients.php
@@ -8,7 +8,6 @@ $dbname = "physio_app";
 $dbh = mysqli_connect($host, $uname, $pass, $dbname) or die("Cannot connect to the database.");
 mysqli_set_charset($dbh, "utf8");
 
-// Query to fetch patient details
 $sqlPatient = "SELECT CONCAT(first_name, ' ', last_name) AS name, age, address FROM patients WHERE soc_sec_reg_num = '287261045727'";
 $resultPatient = mysqli_query($dbh, $sqlPatient);
 
@@ -18,24 +17,28 @@ if (!$resultPatient) {
 
 $rowPatient = mysqli_fetch_assoc($resultPatient);
 
-// Query to fetch session details
-$sqlSession = "SELECT date, hours, notes FROM sessions WHERE id='1'";
+$data = array(
+    "name" => $rowPatient['name'],
+    "age" => $rowPatient['age'],
+    "address" => $rowPatient['address'],
+    "sessions" => array()
+);
+
+$sqlSession = "SELECT date, hours, notes FROM sessions WHERE patient_id = '287261045727' ORDER BY date";
 $resultSession = mysqli_query($dbh, $sqlSession);
 
 if (!$resultSession) {
     die("Query execution failed: " . mysqli_error($dbh));
 }
 
-$rowSession = mysqli_fetch_assoc($resultSession);
-
-$data = array(
-    "name" => $rowPatient['name'],
-    "age" => $rowPatient['age'],
-    "address" => $rowPatient['address'],
-    "date" => $rowSession['date'],
-    "hours" => $rowSession['hours'],
-    "notes" => $rowSession['notes']
-);
+while ($rowSession = mysqli_fetch_assoc($resultSession)) {
+    $session = array(
+        "date" => $rowSession['date'],
+        "hours" => $rowSession['hours'],
+        "notes" => $rowSession['notes']
+    );
+    $data['sessions'][] = $session;
+}
 
 $response = json_encode($data, JSON_UNESCAPED_UNICODE);
 
