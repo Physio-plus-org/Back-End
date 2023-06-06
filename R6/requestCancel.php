3fdb9cbe3e9a99a7580e7871810235bbaf1adf69
spyridStel <105450649+spyridStel@users.noreply.github.com>
2023-06-06 17:07:11 +0300

    backend php

---
+++ R6/requestCancel.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+
+$id = $_GET["id"];
+
+
+// Connect Information
+$host = "localhost";
+$uname = "root";
+$pass = "";
+$dbname = "physio_plus";
+$dbh = mysqli_connect($host, $uname, $pass) or die("cannot connect");
+$dbh->set_charset("utf8");
+
+mysqli_select_db($dbh, $dbname);
+
+$sql = "UPDATE `requests` SET `status` = 'CANCELED' WHERE `requests`.`id` = $id";
+$result = mysqli_query($dbh, $sql);
+echo $result;
+
+mysqli_close($dbh);
+
+
+?>
