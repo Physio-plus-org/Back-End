d1f7e915382bbace8373a282b3b863ccdc2a0a36
ChrisGiamakidis <87308690+ChrisGiamakidis@users.noreply.github.com>
2023-06-18 20:06:09 +0300

    changeConnection

--- R2/request.php
+++ R2/request.php
@@ -1,19 +1,15 @@
 <?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "physio_plus";
+    require("../Utils/dbconnection.php");
     
-    $conn = new mysqli($host, $username, $password, $dbname);
-    if (!$conn) {
-         die("Connection failed: " . $conn->connect_error);
-    }
-
-    $title = $_POST["title"];
-    $description = $$title = $_POST["desc"];
-    $code = $$title = $_POST["code"];
-    $cost = $$title = $_POST["cost"];
-    echo $title." ". $description ." ". $code . " " . $cost;
-    $sql = 'INSERT INTO services (title, description, code, cost) VALUES ("'.$title.'", "'.$description.'", "'.$code.'", '.$cost.')';
-    $result = mysqli_query($conn, $sql);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $title = $_POST["title"];
+        $description = $$title = $_POST["desc"];
+        $code = $$title = $_POST["code"];
+        $cost = $$title = $_POST["cost"];
+        $db = new DBConnection();
+        if ($db->connect()) {
+            $sql = 'INSERT INTO services (title, description, code, cost) VALUES ("'.$title.'", "'.$description.'", "'.$code.'", '.$cost.')';
+            $result = mysqli_query($conn, $sql);
+        }
+    }    
 ?>
