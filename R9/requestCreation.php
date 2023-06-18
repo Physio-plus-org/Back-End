603542962b03f1ab62c432e351b5f9d949e55300
Eleftheria Taggili <taggili.eleftheria@gmail.com>
2023-06-18 20:22:16 +0300

    Some Changes

--- R9/requestCreation.php
+++ R9/requestCreation.php
@@ -1,4 +1,5 @@
 <?php
+require("../Utils/dbconnection.php");
     session_start();
     $json = file_get_contents('php://input');
 
@@ -11,26 +12,24 @@
     // echo $request->status;
 
 
-    $host = "localhost";
-    $uname = "root";
-    $pass = "";
-    $dbname = "physio_plus";
-    $dbh = mysqli_connect($host, $uname, $pass) or die("cannot connect");
-    $dbh->set_charset("utf8");
+    // $host = "localhost";
+    // $uname = "root";
+    // $pass = "";
+    // $dbname = "physio_plus";
+    // $dbh = mysqli_connect($host, $uname, $pass) or die("cannot connect");
+    // $dbh->set_charset("utf8");
 
-    mysqli_select_db($dbh, $dbname);
+    // mysqli_select_db($dbh, $dbname);
     
 
     $id = rand(100, 999999);
+    $db = new DBConnection();
+    if ($db->connect()) {
+        $sql = "INSERT INTO `requests` (`id`, `physio_center`, `patient_id`, `date_time`, `status`) 
+         VALUES ('$id', '$request->physio_center', '$request->patient_id', '$request->requestedDate', '$request->status')";
+         $result = mysqli_query($dbh, $sql);
+         echo $request;
+    }
+    $db->close();
 
-    $sql = "INSERT INTO `requests` (`id`, `physio_center`, `patient_id`, `date_time`, `status`) 
-            VALUES ('$id', '$request->physio_center', '$request->patient_id', '$request->requestedDate', '$request->status')";
-    
-
-    $result = mysqli_query($dbh, $sql);
-    echo $request;
-
-
-
-    mysqli_close($dbh);
 ?>
--- R9/requestLoad.php
+++ R9/requestLoad.php
@@ -1,4 +1,5 @@
 <?php
+require("../Utils/dbconnection.php");
     session_start();
     $id = 000;
     $physio_center = 000;
@@ -14,43 +15,46 @@
     $range_start = $_GET["range_start"];
     $range_end = $_GET["range_end"];
 
-
-    
-
-    
-    $host = "localhost";
-    $uname = "root";
-    $pass = "";
-    $dbname = "physio_plus";
-    $dbh = mysqli_connect($host, $uname, $pass) or die("cannot connect");
-    $dbh->set_charset("utf8");
-
-    mysqli_select_db($dbh, $dbname);
-
-    $sql = "SELECT * FROM `requests` WHERE `date_time` BETWEEN '$range_start' AND '$range_end';";
-    $result = mysqli_query($dbh, $sql);
-    while ($row = mysqli_fetch_array($result)) {
-        $request = new Request();
-        $request->id = $row["id"]; 
-        $request->physio_center = $row["physio_center"];
-        $request->patient_id = $row["patient_id"];
-        $request->date_time = $row["date_time"];
-        array_push($requests, $request);
-        // $requests[count($requests)] = $request;
-        
+    $db = new DBConnection();
+    if ($db->connect()) {
+        $sql = "SELECT * FROM `requests` WHERE `date_time` BETWEEN '$range_start' AND '$range_end';";
+        $result = mysqli_query($dbh, $sql);
+        while ($row = mysqli_fetch_array($result)) {
+            $request = new Request();
+            $request->id = $row["id"]; 
+            $request->physio_center = $row["physio_center"];
+            $request->patient_id = $row["patient_id"];
+            $request->date_time = $row["date_time"];
+            array_push($requests, $request);
+            // $requests[count($requests)] = $request;
+            
+        }
     }
-    mysqli_close($dbh);
+    $db->close();
+    // $host = "localhost";
+    // $uname = "root";
+    // $pass = "";
+    // $dbname = "physio_plus";
+    // $dbh = mysqli_connect($host, $uname, $pass) or die("cannot connect");
+    // $dbh->set_charset("utf8");
+
+    // mysqli_select_db($dbh, $dbname);
+
+    // $sql = "SELECT * FROM `requests` WHERE `date_time` BETWEEN '$range_start' AND '$range_end';";
+    // $result = mysqli_query($dbh, $sql);
+    // while ($row = mysqli_fetch_array($result)) {
+    //     $request = new Request();
+    //     $request->id = $row["id"]; 
+    //     $request->physio_center = $row["physio_center"];
+    //     $request->patient_id = $row["patient_id"];
+    //     $request->date_time = $row["date_time"];
+    //     array_push($requests, $request);
+    //     // $requests[count($requests)] = $request;
+        
+    // }
+    // mysqli_close($dbh);
     // $obj = (object)$requests;
     $encoded = json_encode($requests);
     echo $encoded;
     
-
-    
-
-
-
-
-
-
-
 ?>
