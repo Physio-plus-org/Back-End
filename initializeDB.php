3da1bd09c9f08096851c17f0055b1a23c2739d0e
Kostas Thomson <kostasthomson12@gmail.com>
2023-06-18 17:42:11 +0300

    Update initializeDB.php

--- initializeDB.php
+++ initializeDB.php
@@ -1,38 +1,12 @@
 <?php
-require('../Utils/database.php');
+require('./Utils/database.php');
 
-function generateRandomString($length = 25) {
-   $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-   $charactersLength = strlen($characters);
-   $randomString = '';
-   for ($i = 0; $i < $length; $i++) {
-      $randomString .= $characters[rand(0, $charactersLength - 1)];
-   }
-   return $randomString;
-}
  $db = new DBConnection();
  if($db->connect()){
     echo "connected";
+    $db->show();
  }else{
     echo "not connected";
  }
  $db->close();
-//  $ids = array();
-//  for( $i = 0; $i < 10; $i++) {
-//    $id = rand(1000, 9999) . rand(1000, 9999) . rand(1000, 9999);
-//    $fname = generateRandomString(4);
-//    $lname = generateRandomString(4);
-//    $act = $i;
-//    $ids[$id] = $fname.",".$lname.",".$act;
-//    echo $id . $ids[$id] ."<br>";
-//  }
-//  foreach ( $ids as $id=>$value) {
-//    $split = explode(",", $value);
-//    $query = "INSERT INTO patients (personal_number, first_name, last_name, actions) VALUES ('".$id."','".$split[0]."','".$split[1]."','".$split[2]."');";
-//    try {
-//       $conn->prepare($query)->execute();
-//    } catch (Exception $e) {
-//      echo "Error executing query. Information: ".$e->getMessage();
-//    }
-//  }
   ?>
