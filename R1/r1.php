d76546b4c28344a953b86ed96405219e69d7bae1
Efstathios Iosifidis <iefstathios@gmail.com>
2023-06-18 18:39:04 +0300

    dbconnection.php added and changed the file accordingly. Seems it works

--- R1/r1.php
+++ R1/r1.php
@@ -1,49 +1,39 @@
 
 <?php
+// Requirements
+require("../Utils/dbconnection.php");
+
 // define variables and set to empty values
 $data = array();
 $namePhysio = $_POST["name"];
 $addressPhysio = $_POST["address"];
 $afmPhysio = $_POST["tax_id_number"];
-
-
-
-// Replace with your own MySQL database credentials
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "physio_plus";
 $tabname = "physio_centers";
 
-session_start();
-
 // Create a connection to the database
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check if the connection was successful
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+$db = new DBConnection();
+$db->connect();
+$conn = $db->DB_CONN_OBJ;
  
 // Check if tax ID number already exists in the database
 $checkQuery = "SELECT * FROM $tabname WHERE tax_id_number = '$afmPhysio'";
-$result = mysqli_query($conn, $checkQuery);
+$result = $db->query($checkQuery);
 
-if (mysqli_num_rows($result) > 0) {
+if ($result->num_rows > 0) {
     // Tax ID number already exists in the database
     echo "Tax ID number already exists";
 } else {
     // Insert the data into the database
     $insertQuery = "INSERT INTO $tabname (tax_id_number, name, address) VALUES ('$afmPhysio', '$namePhysio', '$addressPhysio')";
   
-    if (mysqli_query($conn, $insertQuery)) {
+    if ($db->query($insertQuery)) {
         echo "Data saved successfully";
     } else {
-        echo "Error: " . $insertQuery . "<br>" . mysqli_error($conn);
+        echo "Error: " . $insertQuery . "<br>" . $conn->error;
     }
 }
 
 // Close the database connection
-mysqli_close($conn);
+$db->close();
 
 ?>
