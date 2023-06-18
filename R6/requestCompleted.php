9c5ce85a3e6447e7df38d6224295ed45489f9678
spyridStel <spyridopste@gmail.com>
2023-06-18 23:03:53 +0300

    Changes

--- R6/requestCompleted.php
+++ R6/requestCompleted.php
@@ -1,24 +1,18 @@
 <?php
-session_start();
-
+require("../Utils/dbconnection.php");
 
 $id = $_GET["id"];
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-// Connect Information
-$host = "localhost";
-$uname = "root";
-$pass = "";
-$dbname = "physio_plus";
-$dbh = mysqli_connect($host, $uname, $pass) or die("cannot connect");
-$dbh->set_charset("utf8");
+    $db = new DBConnection();
+    if ($db->connect()) {
+        $sql = "UPDATE `requests` SET `status` = 'COMPLETED' WHERE `requests`.`id` = $id";
+        $result = $db->query($sql);
+        echo $result;
+            
+    }
 
-mysqli_select_db($dbh, $dbname);
-
-$sql = "UPDATE `requests` SET `status` = 'COMPLETED' WHERE `requests`.`id` = $id";
-$result = mysqli_query($dbh, $sql);
-echo $result;
-
-mysqli_close($dbh);
-
+    $db->close();
+}
 ?>
--- R6/requestEvents.php
+++ R6/requestEvents.php
@@ -1,49 +1,32 @@
 <?php
-    session_start();
-    // Default Values
-    $id = 000;
-    $physio_center = 000;
-    $patient_id = 000;
-    $date_time = '2023-01-01';
-    $status = 'UPCOMING';
+    require('../Utils/dbconnection.php');
 
-    class Event {
-        
+    class Event {  
     }
     
-    $requests = array();
-
     $range_start = $_GET["range_start"];
     $range_end = $_GET["range_end"];
     $status = $_GET["status"];
 
-
-    // Connect Information
-    $host = "localhost";
-    $uname = "root";
-    $pass = "";
-    $dbname = "physio_plus";
-    $dbh = mysqli_connect($host, $uname, $pass) or die("cannot connect");
-    $dbh->set_charset("utf8");
-
-    mysqli_select_db($dbh, $dbname);
-
-    $sql = "SELECT *  FROM `requests` WHERE `date_time` BETWEEN '$range_start' AND '$range_end' AND `status` = '$status';";
-
-    $result = mysqli_query($dbh, $sql);
-    while ($row = mysqli_fetch_array($result)) {
-        $event = new Event();
-        $event->id = $row["id"]; 
-        $event->physio_center = $row["physio_center"];
-        $event->patient_id = $row["patient_id"];
-        $event->date_time = $row["date_time"];
-        $event->status = $row['status'];
-        array_push($requests, $event);
-        // $requests[count($requests)] = $event;
-        
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $db = new DBConnection();
+        if ($db->connect()) {
+            $sql = "SELECT *  FROM `requests` WHERE `date_time` BETWEEN '$range_start' AND '$range_end' AND `status` = '$status';";
+            $result = $db->query($sql);
+            if ($result->num_rows > 0) {
+                $requests = array();
+                while($row = $result->fetch_assoc()) {
+                    $event = new Event();
+                    $event->id = $row["id"]; 
+                    $event->physio_center = $row["physio_center"];
+                    $event->patient_id = $row["patient_id"];
+                    $event->date_time = $row["date_time"];
+                    $event->status = $row['status'];
+                    array_push($requests, $row); 
+                }
+                echo json_encode($requests);
+            }
+        }
+        $db->close();
     }
-    mysqli_close($dbh);
-    // $obj = (object)$requests;
-    $encoded = json_encode($requests);
-    echo $encoded;
 ?>
