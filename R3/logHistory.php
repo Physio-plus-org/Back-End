511afa0df2f19e457a0bbbb13dfd1917a531a7ec
ElisavetKanidou <elisavetkanidou@gmail.com>
2023-06-19 17:45:00 +0300

    loghistory
    loghistory

--- R3/logHistory.php
+++ R3/logHistory.php
@@ -2,10 +2,10 @@
 require("../Utils/dbconnection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $namep = $_POST["first_name"];
-    $surname = $_POST["last_name"];
-    $soc_sec_reg_num = $_POST["ssrn"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
     $address = $_POST["address"];
+    $ssrn = $_POST["ssrn"];
     $db = new DBConnection();
     if ($db->connect()) {
         $sql = "INSERT INTO patients (first_name, last_name, address, ssrn) VALUES ('".$first_name."','".$last_name."','".$address."','".$ssrn."');";
