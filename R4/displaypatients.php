211dd8ed5df81343220208d4bd37a6a3a4479f01
NikosSakell <nikossakell02@gmail.com>
2023-06-20 14:20:53 +0300

    php changes

--- R4/displaypatients.php
+++ R4/displaypatients.php
@@ -1,55 +1,30 @@
 <?php
+require('../Utils/dbconnection.php');
 
-$host = "localhost";
-$uname = "id20930892_physio_user";
-$pass = "Physio__Plus_User2023";
-$dbname = "id20930892_physio_plus";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $db = new DBConnection();
 
-$dbh = mysqli_connect($host, $uname, $pass, $dbname) or die("Cannot connect to the database.");
-mysqli_set_charset($dbh, "utf8");
+    if ($db->connect()) {
+        $ssrn = $_POST['amka'];
 
-// Retrieve the patientName variable from the Java class
-$patientName = $_POST['patientName'];
+        $sql = "SELECT CONCAT(first_name, ' ', last_name) AS name, address FROM patients WHERE ssrn = '$ssrn'";
+        $result = $db->query($sql);
 
-// Use mysqli_real_escape_string to prevent SQL injection
-$escapedPatientName = mysqli_real_escape_string($dbh, $patientName);
+        if ($result->num_rows > 0) {
+            $output = array();
+            while ($row = $result->fetch_assoc()) {
+                array_push($output, $row);
+            }
 
-$sqlPatient = "SELECT CONCAT(first_name, ' ', last_name) AS name, age, address FROM patients WHERE name = '$escapedPatientName'";
-$resultPatient = mysqli_query($dbh, $sqlPatient);
+            $json = json_encode($output);
+            echo $json;
+        } else {
+            echo "No records found.";
+        }
+    } else {
+        echo "Not connected";
+    }
 
-if (!$resultPatient) {
-    die("Query execution failed: " . mysqli_error($dbh));
+    $db->close();
 }
-
-$rowPatient = mysqli_fetch_assoc($resultPatient);
-
-$data = array(
-    "name" => $rowPatient['name'],
-    "age" => $rowPatient['age'],
-    "address" => $rowPatient['address'],
-    "sessions" => array()
-);
-
-$sqlSession = "SELECT date, hours, notes FROM sessions WHERE patient_id = '287261045727' ORDER BY date";
-$resultSession = mysqli_query($dbh, $sqlSession);
-
-if (!$resultSession) {
-    die("Query execution failed: " . mysqli_error($dbh));
-}
-
-while ($rowSession = mysqli_fetch_assoc($resultSession)) {
-    $session = array(
-        "date" => $rowSession['date'],
-        "hours" => $rowSession['hours'],
-        "notes" => $rowSession['notes']
-    );
-    $data['sessions'][] = $session;
-}
-
-$response = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-echo $response;
-
-mysqli_close($dbh);
-
 ?>
