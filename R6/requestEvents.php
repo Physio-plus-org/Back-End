63acbe4f636305312c9c29c4e685b886b0d784c3
Kostas Thomson <kostasthomson12@gmail.com>
2023-06-23 17:01:15 +0300

    Update requestEvents.php

--- R6/requestEvents.php
+++ R6/requestEvents.php
@@ -1,25 +1,28 @@
 <?php
     require('../Utils/dbconnection.php');
 
-    class Event {  
-    }
-    
-    $range_start = $_GET["range_start"];
-    $range_end = $_GET["range_end"];
-    $status = $_GET["status"];
-
-
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $p_id = $_POST["patient_id"];
-        $s_id = $_POST["service_id"];
-        $date_time = $_POST["date_time"];
+        $range_start = $_POST["range_start"];
+        $range_end = $_POST["range_end"];
         $status = $_POST["status"];
         $db = new DBConnection();
         if ($db->connect()) {
-            $sql = "INSERT INTO sessions (patient_id, service_id, date, status) VALUES ('".$p_id."','".$s_id."','".$date."','".$note."');";
+            $sql = "SELECT *  FROM `requests` WHERE `date_time` BETWEEN '$range_start' AND '$range_end' AND `status` = '$status';";
             $result = $db->query($sql);
+            if ($result->num_rows > 0) {
+                $requests = array();
+                while($row = $result->fetch_assoc()) {
+                    // $event = new Event();
+                    // $event->id = $row["id"]; 
+                    // $event->physio_center = $row["physio_center"];
+                    // $event->patient_id = $row["patient_id"];
+                    // $event->date_time = $row["date_time"];
+                    // $event->status = $row['status'];
+                    array_push($requests, $row); 
+                }
+                echo json_encode($requests);
+            }
         }
         $db->close();
     }
-    ?>
 ?>
